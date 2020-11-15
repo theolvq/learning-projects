9beff42076470694cf9ae7c35143097d2d0cec3b
Daawa-Karma <theo.leveque@gmail.com>
2020-11-14 21:48:54 -0800

    works fine

--- js/game.js
+++ js/game.js
@@ -1,11 +1,31 @@
 const container = document.querySelector(".container");
 const btnReset = document.querySelector(".btn-reset");
 btnResize = document.querySelector(".btn-resize");
+const pixel = document.querySelectorAll(".square");
 let gridSize = 16;
+
+// change square color
+function changeColor(e) {
+  e.classList.add("square-black");
+}
+
+// create grid
+function createGrid(size) {
+  container.innerHTML = "";
+  container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+  for (let i = 0; i < size * size; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    square.addEventListener("mouseenter", () => changeColor(square));
+    container.appendChild(square);
+  }
+}
+
+window.addEventListener("DOMContentLoaded", createGrid(gridSize));
 
 //resize grid
 btnResize.addEventListener("click", function () {
-  pixel.forEach((p) => p.remove());
   do {
     gridSize = parseInt(prompt("How many squares per side?"));
   } while (gridSize >= 100);
@@ -15,29 +35,4 @@ btnResize.addEventListener("click", function () {
 //reset grid
 btnReset.addEventListener("click", function () {
   pixel.forEach((p) => p.classList.remove("square-black"));
-});
-
-// create grid
-function createGrid(size) {
-  if (size <= 100) {
-    for (let i = 0; i < size * size; i++) {
-      const square = document.createElement("div");
-      square.classList.add("square");
-      container.appendChild(square);
-    }
-    container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
-    container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
-  }
-}
-
-window.addEventListener("DOMContentLoaded", createGrid(gridSize));
-
-const pixel = document.querySelectorAll(".square");
-
-// change square color
-pixel.forEach((p) => {
-  p.addEventListener("mouseenter", function (e) {
-    let square = e.target;
-    square.classList.add("square-black");
-  });
 });
