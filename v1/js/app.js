3e4ed95b65c668875eeb81e1ce468790568b7b0b
Daawa-Karma <theo.leveque@gmail.com>
2020-12-18 10:04:41 -0800

    reoganized directory and started v2

---
+++ v1/js/app.js
@@ -0,0 +1,65 @@
+//button selection
+const btns = document.querySelectorAll('.btn');
+const digits = document.querySelectorAll('.operand');
+const operator = document.querySelectorAll('.operator');
+const equal = document.querySelector('.equal');
+const clear = document.querySelector('.clear');
+const display = document.querySelector('.display');
+
+// operation functions
+const add = (a, b) => parseInt(a) + parseInt(b);
+const sub = (a, b) => parseInt(a) - parseInt(b);
+const mult = (a, b) => parseInt(a) * parseInt(b);
+const div = (a, b) => parseInt(a) / parseInt(b);
+const operate = (n1, n2, operator) => {
+	let result;
+	if (operator === '+') {
+		result = add(n1, n2);
+	} else if (operator === '-') {
+		result = sub(n1, n2);
+	} else if (operator === '*') {
+		result = mult(n1, n2);
+	} else if (operator == '/') {
+		result = div(n1, n2);
+	}
+	if (result.toString().includes('.')) {
+		display.textContent = result.toFixed(6);
+	} else {
+		display.textContent = result;
+	}
+	return result;
+};
+
+//capture button data
+let op;
+operator.forEach(o =>
+	o.addEventListener('click', () => {
+		op = o.dataset.id;
+	})
+);
+
+let num1;
+let num2;
+digits.forEach(d =>
+	d.addEventListener('click', () => {
+		if (!num1) {
+			num1 = d.dataset.id;
+			display.textContent = num1;
+		} else if (num1) {
+			num2 = d.dataset.id;
+			display.textContent = num2;
+		}
+		if (!op) {
+		}
+	})
+);
+
+equal.addEventListener('click', () => operate(num1, num2, op));
+
+const clearFn = () => {
+	display.textContent = '';
+	num1 = '';
+	num2 = '';
+};
+
+clear.addEventListener('click', () => clearFn());
