8855df76126501084c8e62b3c3d6dd6b3304dc9f
Daawa-Karma <theo.leveque@gmail.com>
2021-02-05 17:52:47 -0800

    object added

--- v2/js/app.js
+++ v2/js/app.js
@@ -1,10 +1,11 @@
 //button selection
 const btns = document.querySelectorAll('.btn')
 const digits = document.querySelectorAll('.operand')
-const operator = document.querySelectorAll('.operator')
+const operators = document.querySelectorAll('.operator')
 const equal = document.querySelector('#equal')
 const clear = document.querySelector('#clear')
 const del = document.querySelector('#delete')
+const result = document.querySelector('.result')
 const display = document.querySelector('.display')
 
 // operation functions
@@ -33,9 +34,38 @@ const operate = (n1, n2, operator) => {
 // Object literal
 const calculator = {}
 
-digits.forEach(digit =>
-	digit.addEventListener('click', () => {
-		calculator.operand = digit.textContent
-		console.log(calculator.operand)
-	})
-)
+//General functions
+const appendNumber = e => {
+	if (!calculator.currentOperand) {
+		calculator.currentOperand = e.target.textContent
+		result.innerText = calculator.currentOperand
+	} else if (calculator.currentOperand) {
+		calculator.currentOperand += e.target.textContent
+		result.innerText = calculator.currentOperand
+	} else if (calculator.operator) {
+		calculator.previousOperand = calculator.currentOperand
+		calculator.currentOperand = e.target.textContent
+		display.innerText = calculator.previousOperand
+		result.innerText = ''
+	}
+}
+
+const getOperator = e => {
+	if (!calculator.operator) {
+		calculator.operator = e.target.textContent
+	}
+	console.log(calculator.operator)
+}
+
+// Event listeners
+digits.forEach(digit => digit.addEventListener('click', appendNumber))
+
+operators.forEach(operator => operator.addEventListener('click', getOperator))
+
+equal.addEventListener('click', () => {
+	operate(
+		calculator.previousOperand,
+		calculator.currentOperand,
+		calculator.operator
+	)
+})
