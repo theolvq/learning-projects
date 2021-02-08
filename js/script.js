805563f16bb94dbca6efd39392e7b2ee1377b4a9
Daawa-Karma <theo.leveque@gmail.com>
2021-02-07 20:17:20 -0800

    overlay added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 let myLibrary = []
-const btn = document.querySelector('input[type="button"]')
+const submitBtn = document.querySelector('input[type="button"]')
+const addBtn = document.querySelector('.add-btn')
+const overlay = document.querySelector('#overlay')
+const form = document.querySelector('#form')
 
 class Book {
 	constructor(title, author, pages, status) {
@@ -17,13 +20,27 @@ class Book {
 
 const addToLibrary = () => {}
 
+const openForm = () => {
+	overlay.classList.remove('overlay-hidden')
+	overlay.classList.add('overlay')
+}
+
+const escapeOverlay = () => {
+	if (!overlay.classList.contains('hidden')) {
+		overlay.classList.remove('overlay')
+		overlay.classList.add('overlay-hidden')
+	}
+}
+
 const createBook = () => {
 	const bookTitle = document.querySelector('input[name="title"]').value
 	const bookAuthor = document.querySelector('input[name="author"]').value
 	const bookPages = document.querySelector('input[name="pages"]').value
 	const bookStatus = document.querySelector('input[name="read-status"]')
 		.checked
+
 	myLibrary.push(new Book(bookTitle, bookAuthor, bookPages, bookStatus))
 }
-
-btn.addEventListener('click', createBook)
+addBtn.addEventListener('click', openForm)
+submitBtn.addEventListener('click', createBook)
+overlay.addEventListener('click', escapeOverlay)
