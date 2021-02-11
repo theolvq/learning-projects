bd642f38bcfe8cada136fc2005048b22b4efc807
Daawa-Karma <theo.leveque@gmail.com>
2021-02-10 21:35:19 -0800

    basic library styling

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
-let myLibrary = []
 const submitBtn = document.querySelector('.submit-btn')
 const addBtn = document.querySelector('.add-btn')
 const overlay = document.querySelector('#overlay')
 const form = document.querySelector('#form')
+const confirmation = document.querySelector('#confirmation')
+
+let myLibrary = []
+// let myLibrarySerialized = JSON.stringify(myLibrary)
+
+if (!localStorage.setItem('myLibrary', JSON.stringify(myLibrary)))
+	localStorage.setItem('myLibrary', JSON.stringify(myLibrary))
+
+// let myLibraryDeserialized = JSON.parse(localStorage.getItem('myLibrary'))
 
 class Book {
 	constructor(title, author, pages, status) {
@@ -18,7 +26,18 @@ class Book {
 	}
 }
 
-const addToLibrary = () => {}
+const addToLibrary = () => {
+	const bookTitle = document.querySelector('input[name="title"]').value
+	const bookAuthor = document.querySelector('input[name="author"]').value
+	const bookPages = document.querySelector('input[name="pages"]').value
+	const bookStatus = document.querySelector('input[name="read-status"]')
+		.checked
+	myLibrary = JSON.parse(localStorage.getItem('myLibrary'))
+	myLibrary.push(new Book(bookTitle, bookAuthor, bookPages, bookStatus))
+	localStorage.setItem('myLibrary', JSON.stringify(myLibrary))
+	confirmation.innerText = 'Book Added!'
+	console.log(localStorage)
+}
 
 const openForm = () => {
 	overlay.classList.remove('overlay-hidden')
@@ -26,21 +45,22 @@ const openForm = () => {
 }
 
 const escapeOverlay = e => {
-	if (e.target === overlay && overlay.classList.contains('hidden')) {
+	if (e.target === overlay && !overlay.classList.contains('hidden')) {
+		overlay.classList.remove('overlay')
+		overlay.classList.add('overlay-hidden')
+	} else if (e.target === submitBtn) {
+		e.preventDefault()
 		overlay.classList.remove('overlay')
 		overlay.classList.add('overlay-hidden')
 	}
 }
 
-const createBook = () => {
-	const bookTitle = document.querySelector('input[name="title"]').value
-	const bookAuthor = document.querySelector('input[name="author"]').value
-	const bookPages = document.querySelector('input[name="pages"]').value
-	const bookStatus = document.querySelector('input[name="read-status"]')
-		.checked
+const createBook = () => {}
 
-	myLibrary.push(new Book(bookTitle, bookAuthor, bookPages, bookStatus))
-}
 addBtn.addEventListener('click', openForm)
-submitBtn.addEventListener('click', createBook)
+submitBtn.addEventListener('click', () => {
+	addToLibrary()
+	createBook()
+	escapeOverlay(submitBtn)
+})
 overlay.addEventListener('click', escapeOverlay)
