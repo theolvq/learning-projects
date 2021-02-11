947e5b43c5375791e602d6bbf63b30d8a4dc201a
Daawa-Karma <theo.leveque@gmail.com>
2021-02-10 22:29:44 -0800

    local storage sorted

--- js/script.js
+++ js/script.js
@@ -6,10 +6,6 @@ const confirmation = document.querySelector('#confirmation')
 
 let myLibrary = []
 // let myLibrarySerialized = JSON.stringify(myLibrary)
-
-if (!localStorage.setItem('myLibrary', JSON.stringify(myLibrary)))
-	localStorage.setItem('myLibrary', JSON.stringify(myLibrary))
-
 // let myLibraryDeserialized = JSON.parse(localStorage.getItem('myLibrary'))
 
 class Book {
@@ -26,16 +22,24 @@ class Book {
 	}
 }
 
+const getMyLibrary = () => JSON.parse(localStorage.getItem('myLibrary'))
+
+const setMyLibrary = () =>
+	localStorage.setItem('myLibrary', JSON.stringify(myLibrary))
+
+if (localStorage.getItem('myLibrary') === null) setMyLibrary()
+
 const addToLibrary = () => {
 	const bookTitle = document.querySelector('input[name="title"]').value
 	const bookAuthor = document.querySelector('input[name="author"]').value
 	const bookPages = document.querySelector('input[name="pages"]').value
 	const bookStatus = document.querySelector('input[name="read-status"]')
 		.checked
-	myLibrary = JSON.parse(localStorage.getItem('myLibrary'))
+	myLibrary = getMyLibrary()
 	myLibrary.push(new Book(bookTitle, bookAuthor, bookPages, bookStatus))
-	localStorage.setItem('myLibrary', JSON.stringify(myLibrary))
+	setMyLibrary()
 	confirmation.innerText = 'Book Added!'
+	window.setTimeout((confirmation.innerText = ''), 3000)
 	console.log(localStorage)
 }
 
@@ -55,7 +59,18 @@ const escapeOverlay = e => {
 	}
 }
 
-const createBook = () => {}
+const createBook = () => {
+	myLibrary = getMyLibrary()
+	const currentBook = myLibrary[myLibrary.length - 1]
+	const card = document.createElement('div')
+	const libraryContainer = document.querySelector('.library-container')
+	libraryContainer.appendChild(card)
+	card.classList.add('card')
+	card.innerHTML = `<h3 class="card-title">${currentBook.title}</h3>
+	<h3 class="card-author">${currentBook.author}</h3>
+	<h3 class="card-pages">${currentBook.pages}</h3>
+	<h3 class="card-read-status">${currentBook.status}</h3>`
+}
 
 addBtn.addEventListener('click', openForm)
 submitBtn.addEventListener('click', () => {
