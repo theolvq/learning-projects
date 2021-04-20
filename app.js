16cfc4ed3e13b97e717d623c10e92b64fe8891eb
Theo-Leveque <theo.leveque@gmail.com>
2021-04-19 21:02:00 -0700

    async/await done

--- app.js
+++ app.js
@@ -1,21 +1,37 @@
-//npm - global command, comes with node
-//npm --version
+const { readFile, writeFile } = require('fs').promises;
+const util = require('util');
+// const readFilePromise = util.promisify(readFile);
+// const writeFilePromise = util.promisify(writeFile);
+//
+const start = async () => {
+  try {
+    const first = await readFile('./content/first.txt', 'utf8');
+    const second = await readFile('./content/second.txt', 'utf8');
+    await writeFile(
+      './content/result-mind-grenade.txt',
+      `This is Awesome: ${first} \n${second} `
+    );
+    console.log(first, second);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-//local dependency - use it only in this particular project
-//npm i <packageName>
+start();
+// const getText = path => {
+//   return new Promise((resolve, reject) => {
+//     readFile(path, 'utf8', (err, data) => {
+//       if (err) {
+//         reject(err);
+//       } else {
+//         resolve(data);
+//       }
+//     });
+//   });
+// };
 
-//global dependency - use it in any project
-//npm install -g <packageName>
-//sudo instal -g <packageName> (mac)
+//
 
-//package.json - manifest file(stores important info about project/package)
-//manual approach (create package.json in the root, create properties etc...)
-//npm init (step by step, press enter to skip)
-//npm init -y (everything default)
-
-const _ = require('lodash');
-
-const items = [1, [2, [3, [4]]]];
-
-const newItems = _.flattenDeep(items);
-console.log(newItems);
+// getText('./content/first.txt')
+//   .then(result => console.log(result))
+//   .catch(err => console.log(err));
