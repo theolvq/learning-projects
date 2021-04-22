d14ef5e63213b4f23cb2237fc607f729d5560cc2
Theo-Leveque <theo.leveque@gmail.com>
2021-04-21 22:33:22 -0700

    express basics completed

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,39 +1,6 @@
-const http = require('http');
-const { readFileSync } = require('fs');
+const express = require('express');
+const app = express();
 
-//get all files
-const homePage = readFileSync('./navbar-app/index.html');
-const homeStyles = readFileSync('./navbar-app/styles.css');
-const homeImage = readFileSync('./navbar-app/logo.svg');
-const homeLogic = readFileSync('./navbar-app/browser-app.js');
-
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  if (url === '/') {
-    res.writeHead(200, { 'content-type': 'text/html' });
-    res.write(homePage);
-    res.end();
-  } else if (url === '/styles.css') {
-    res.writeHead(200, { 'content-type': 'text/css' });
-    res.write(homeStyles);
-    res.end();
-  } else if (url === '/logo.svg') {
-    res.writeHead(200, { 'content-type': 'image/svg+xml' });
-    res.write(homeImage);
-    res.end();
-  } else if (url === '/browser-app.js') {
-    res.writeHead(200, { 'content-type': 'text/javascript' });
-    res.write(homeImage);
-    res.end();
-  } else if (url === '/about') {
-    res.writeHead(200, { 'content-type': 'text/html' });
-    res.write('<h1>about page</h1>');
-    res.end();
-  } else {
-    res.writeHead(404, { 'content-type': 'text/html' });
-    res.write('<h1>error page</h1>');
-    res.end();
-  }
+app.listen(5000, () => {
+  console.log('Server listening on port 5000');
 });
-
-server.listen(5000);
